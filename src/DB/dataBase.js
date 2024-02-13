74e8d8a3a9f216a8458fddced5000bc3646039a2
muzair78 <inoxentali120@gmail.com>
2024-02-13 18:30:33 +0500

    first commit

--- src/DB/dataBase.js
+++ src/DB/dataBase.js
@@ -1,11 +1,10 @@
 const mongoose = require("mongoose");
 
 mongoose
-  // .connect("mongodb://localhost:27017/"), {
-  //   useNewUrlParser: true,
-  //   useUnifiedTopology: true,
-  // })
-  .connect("mongodb://localhost:27017/LeoSitters")
+  .connect(process.env.DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => {
     console.log("Database connected successfully");
   })
--- src/middleware/Auth.js
+++ src/middleware/Auth.js
@@ -1,6 +1,6 @@
 const jwt = require("jsonwebtoken");
 
-const auth = (req, res, next) => {
+const auth = async (req, res, next) => {
   const token = req.headers.authorization;
 
   if (!token) {
@@ -10,7 +10,8 @@ const auth = (req, res, next) => {
   }
 
   try {
-    const decoded = jwt.verify(token, process.env.JWT_KEY);
+    const decoded = await jwt.verify(token, process.env.JWT_KEY);
+    console.log(decoded);
     req.user = decoded;
     if (!decoded) {
       return res.status(401).json({ message: "Authentication failed" });
